a74dcc1e29841b78058ecc9a21d1313597097086
isha_15 <makhijaisha1511@gmail.com>
2024-07-15 15:10:00 +0530

    Added:Dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,6 +8,23 @@ public class SnakeLadder {
         int startPosition = 0;
         int nextPosition=0;
         int dice=0;
+
+        while (startPosition < winningPosition) {
+            dice = random.nextInt(6) + 1;
+            nextPosition = startPosition + dice;
+
+            // Ensure the position does not exceed the winning position
+            if (nextPosition <= winningPosition) {
+                startPosition = nextPosition;
+                System.out.println("Dice Roll: " + dice + ", Position: " + startPosition);
+            }
+
+            // Check for winning condition
+            if (startPosition == winningPosition) {
+                System.out.println("Congratulations! You've won the game!");
+                break;
+            }
+        }
     }
 }
       
