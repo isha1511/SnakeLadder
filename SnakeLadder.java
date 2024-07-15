d51266f45e1696b1c447db7ce2c295aec518e4e8
isha_15 <makhijaisha1511@gmail.com>
2024-07-15 15:05:37 +0530

    Added:start

---
+++ SnakeLadder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+        final int winningPosition = 100;
+        int[] board = new int[winningPosition + 1];
+        int startPosition = 0;
+        int nextPosition=0;
+        int dice=0;
+    }
+}
+      
